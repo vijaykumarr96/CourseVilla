a1b22e98df38a63535476cfd04dc3e4a06f06928
Iamthevk <vijay.vuex@gmail.com>
2023-11-08 17:12:07 +0530

    progress card style fix for mobile view

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # React + Vite
 
+## Getting Started
+
+1. Clone the repository: `git clone https://github.com/Iamthevk/CourseVilla-Alemeno.git`
+
+2. Navigate to the project directory: `cd CourseVilla-Alemeno`
+
+3. Install dependencies: `yarn or yarn install`
+
+4. Run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+
+Open [http://localhost:5173](http://localhost:5173) with your browser to see the result.
+
+## Live Demo
+
+Check out the live demo of the app deployed on [Vercel](https://vercel.com) at [https://course-villa-alemeno-iqwf.vercel.app/](https://course-villa-alemeno-iqwf.vercel.app/).
+
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+```
--- src/components/ProgressCard.jsx
+++ src/components/ProgressCard.jsx
@@ -1,11 +1,9 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ProgressCard = (props) => {
-  const { thumbnail, name, instructor } = props.course;
-  let { id } = useParams();
-
+  const { id, thumbnail, name, instructor } = props.course;
   return (
-    <div className="w-[300px] h-[400px]  md:w-[360px] md:h-[420px] bg-white text-black rounded-2xl px-10 relative">
+    <div className="w-[320px] h-[460px]  md:w-[360px] md:h-[420px] bg-white text-black rounded-2xl px-10 relative">
       <img src={thumbnail} className="h-[30%] mx-auto ptefrf-5 my-2" />
       <div className="mx-7 text-center h-[130px]">
         <h1 className="text-sm md:text-xl font-semibold text-gray-800">
@@ -22,18 +20,18 @@ const ProgressCard = (props) => {
           ></p>
         </div>
       </div>
-      <p className="bg-gray-200 w-[80%] mx-auto h-0.5 mb-5"></p>
-      <div>
+      <p className="bg-gray-200 w-[80%] mx-auto h-0.5 mb-2 md:mb-5"></p>
+      <div className="flex flex-col md:flex-row gap-2 md:gap-5">
         <button
           className={` rounded-xl ${
             !props.isMarked ? "bg-gray-500" : "bg-gray-50 !text-red-600"
-          } text-white text-md absolute left-8 w-[145px] h-12 p-1 leading-4 shadow-lg`}
+          } text-white text-md w-full md:w-[145px] h-12 p-1 leading-4 shadow-lg`}
           onClick={props.handleClick}
         >
           {!props.isMarked ? "Mark as completed" : "Remove complete"}
         </button>
         <Link to={`/course/${id}`}>
-          <button className=" rounded-xl bg-pink-500 text-white text-md absolute right-8 w-[145px] h-12 shadow-lg">
+          <button className=" rounded-xl bg-pink-500 text-white text-md w-full md:w-[145px] h-12 shadow-lg">
             View Course
           </button>
         </Link>
